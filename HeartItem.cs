102b06908f74ca5a1d70a2139c258a973ee09f89
Potat369 <yevheniidemian@gmail.com>
2024-03-29 23:49:36 +0200

    Add GlobalItems for both mana and hearts.

--- HeartItem.cs
+++ HeartItem.cs
@@ -1,11 +1,27 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
 
 namespace CantTakeMyHearts
 {
     internal class HeartItem : GlobalItem
     {
-        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.Heart || entity.type == ItemID.CandyApple || entity.type == ItemID.CandyCane;
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation) 
+        {
+            foreach (ItemDefinition itemDefinition in ModContent.GetInstance<CantTakeMyHeartsConfig>().HeartItems)
+            {
+                if (entity.type == itemDefinition.Type)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override bool CanPickup(Item item, Player player)
+        {
+            return player.statLife < player.statLifeMax2;
+        }
     }
 }
--- ManaItem.cs
+++ ManaItem.cs
@@ -1,12 +1,23 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
 
 namespace CantTakeMyHearts
 {
     internal class ManaItem : GlobalItem
     {
-        public override bool AppliesToEntity(Item entity, bool lateInstantiation) => entity.type == ItemID.Star || entity.type == ItemID.SoulCake || entity.type == ItemID.SugarPlum;
+        public override bool AppliesToEntity(Item entity, bool lateInstantiation) 
+        { 
+            foreach (ItemDefinition itemDefinition in ModContent.GetInstance<CantTakeMyHeartsConfig>().ManaItems)
+            {
+                if (entity.type == itemDefinition.Type)
+                {
+                    return true;
+                }
+            }   
+            return false;
+        }
 
         public override bool CanPickup(Item item, Player player)
         {
